db225c5f13e507c51340e76888f7940d6cb3263a
Kba05 <babahan-98@mail.ru>
2022-06-23 15:55:53 +0300

    new stylize

--- src/Components/Todo.jsx
+++ src/Components/Todo.jsx
@@ -2,26 +2,87 @@ import React from "react";
 
 function Todo(){
     return(
-        <div className="container border rounded mt-1 pb-1">
-            <div className="d-flex">
-                <div className="rounded bg-light bg-gradient fs-4 text fw-lighter">
-                    <p className="text-center fw-normal">Категории</p>
-                    <ul>
-                        <li className="d-block px-3 rounded-start ">По работе</li>
-                        <li className="d-block px-3 rounded-start bg-white">По учебе</li>
-                        <li className="d-block px-3 rounded-start">По дому</li>
-                        <li className="d-block px-3 rounded-start">И другие</li>
-                    </ul>
-                </div>
-                <div  style={{width:1000}} className="border border-2 border-light">
-                    <ul className="px-0 py-0 d-flex  bg-light bg-gradient fs-4 text fw-lighter justify-content-around">
-                        <li className="d-flex rounded-top px-3 bg-white">ToDo</li>
-                        <li className="d-flex rounded-top px-3">Done</li>
-                    </ul>
-                    <div > ToDo</div>
-                    <div> Done</div>
-                </div>
+        <div className="bg-light pt-2" style={{height:1000}}>
+
+            <div className="container">
+
+                <header className="bg-white rounded-3 px-4">
+                    <nav className="navbar navbar-expand-lg  justify-content-between">
+                        <div className="navbar-brand bg-light px-2 rounded-4">
+                            ToDO List
+                        </div>
+                        <div className="navbar-nav">
+                            <ul className="d-flex mx-0 my-0">
+                                <li className="d-flex px-5 rounded-4">do</li>
+                                <li className="d-flex px-5 rounded-4 bg-light">done</li>
+                            </ul>
+                        </div>
+                    </nav>
+                </header>
+
+                <main className="my-2">
+                    <div className="row">
+
+                        <div className="col-3">
+                            <div className="bg-white rounded-3 py-4">
+                                <ul>
+                                    <li className="d-block my-3 py-2 px-2 rounded-start bg-light">
+                                        Home tasks
+                                    </li>
+                                    <li className="d-block my-3 py-2 px-2 rounded-start">
+                                        University tasks
+                                    </li>
+                                    <li className="d-block my-3 py-2 px-2 rounded-start">
+                                        Work tasks
+                                    </li>
+                                    <li className="d-block my-3 py-2 px-2 rounded-start">
+                                        + Add Categoty
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+
+                        <div className="col-9">
+                            <div className="bg-white rounded-2 px-4 py-4">
+                                <div className="input-group mb-3">
+                                    <input  className="form-control"  type="text"placeholder="Recipient's username"/>
+                                    <button className="btn btn-outline-success" type="button" id="button-addon2">Add</button>
+                                </div>
+                                <ul className="list-group">
+                                    <li className="list-group-item">
+                                        <input className="form-check-input me-1" type="checkbox" />
+                                        First checkbox
+                                    </li>
+                                    <li className="list-group-item">
+                                        <input className="form-check-input me-1" type="checkbox" />
+                                        Second checkbox
+                                    </li>
+                                    <li className="list-group-item">
+                                        <input className="form-check-input me-1" type="checkbox" />
+                                        Third checkbox
+                                    </li>
+                                    <li className="list-group-item">
+                                        <input className="form-check-input me-1" type="checkbox" />
+                                        Fourth checkbox
+                                    </li>
+                                    <li className="list-group-item">
+                                        <input className="form-check-input me-1" type="checkbox" />
+                                        Fifth checkbox
+                                    </li>
+                                    <li className="list-group-item">
+                                        <input className="form-check-input me-1" type="checkbox" />
+                                       Sixts checkbox
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+
+                </main>
+               
+
             </div>
+
         </div>
     )
 }
---
+++ src/Components/data.js
@@ -0,0 +1,28 @@
+let todos =[
+    {
+        id:1,
+        text:"clean up at home",
+        isDone: false,
+        category: "home"
+    },
+    {
+        id:2,
+        text:"clean up at home",
+        isDone: false,
+        category: "home"
+    },
+    {
+        id:3,
+        text:"clean up at home",
+        isDone: false,
+        category: "home"
+    },
+    {
+        id:4,
+        text:"clean up at home",
+        isDone: false,
+        category: "home"
+    }
+]
+
+export default todos
