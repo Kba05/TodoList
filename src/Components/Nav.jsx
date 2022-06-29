b9ddfa4392bf0af675651220059edf610447710d
Kba05 <babahan-98@mail.ru>
2022-06-29 17:43:46 +0300

    added css style

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -12,7 +12,7 @@ export const Nav = props => {
             <li 
                 key={stat.id} 
                 className={
-                    `d-block px-5 py-1 rounded-4 ${status===stat.id? "bg-light": ""}`
+                    `d-block px-5 rounded-4 ${status===stat.id? "bg-light": ""}`
                 } 
                 onClick={()=>onClick(stat.id)}> 
                     {stat.text}
@@ -22,12 +22,12 @@ export const Nav = props => {
 
     return(
         <nav className="navbar navbar-expand-lg  justify-content-between">
-            <div className="navbar-brand bg-light px-2 rounded-4">
+            <div className="navbar-brand bg-light px-4 rounded-5">
                 ToDO
             </div>
             <div className="navbar-nav">
-                <button className="btn btn-light btn-sm rounded-4 px-4" onClick={()=>setModalVisible(true)}>+ Add Task</button>
-                <ul className="d-flex mx-0 my-0">
+                <button className="btn btn-light btn-sm rounded-5 px-4 me-5" onClick={()=>setModalVisible(true)}>+ Add Task</button>
+                <ul className="d-flex mx-0 my-0 px-0 py-0 border border-1 rounded-5">
                     {statusElem}
                 </ul>
             </div>
